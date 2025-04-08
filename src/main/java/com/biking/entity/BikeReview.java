13a5bbee43a97526dfa8c02d407d71bdcb9304a4
dharmeshbangaraa <dharmeshbangara@gmail.com>
2025-04-08 20:42:37 +0530

    changed datatype to lob

--- src/main/java/com/biking/entity/BikeReview.java
+++ src/main/java/com/biking/entity/BikeReview.java
@@ -1,9 +1,6 @@
 package com.biking.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
@@ -25,5 +22,6 @@ public class BikeReview {
     private String reviewerEmail;
     private String reviewerRiddenKms;
     private String reviewTitle;
+    @Lob
     private String reviewDescription;
 }
