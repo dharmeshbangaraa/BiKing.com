b8d2114c0664a3704c742fbf557ab79ac0211089
dharmeshbangaraa <dharmeshbangara@gmail.com>
2025-04-24 18:07:08 +0530

    fixing redis repo issue

--- src/main/java/com/biking/BikingApplication.java
+++ src/main/java/com/biking/BikingApplication.java
@@ -3,9 +3,11 @@ package com.biking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableCaching
+@EnableRedisRepositories(basePackages = "com.dummy.package.that.does.not.exist")
 public class BikingApplication {
 
 	public static void main(String[] args) {
